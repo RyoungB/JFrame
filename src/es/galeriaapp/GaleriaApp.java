13455a762f785b15819e9a9a79fa037cd7f14096
Ryoung <roaryoungsl@gmail.com>
2026-06-01 12:52:08 +0200

    Añadiendo abrir explorador de archivos

--- src/es/galeriaapp/GaleriaApp.java
+++ src/es/galeriaapp/GaleriaApp.java
@@ -1,6 +1,7 @@
 package es.galeriaapp;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class GaleriaApp {
 
@@ -8,7 +9,17 @@ public class GaleriaApp {
 		// TODO Auto-generated method stub
 		
 		
-		
+		try {
+			/*
+			 * La siguiente instrcion le dice que pille el aspecto del sistema operativo que tiene mi ordenador 
+			 * 
+			 */
+			
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 		//invokelater inicia Ventana Princiapl en el hilo especial de Swing
--- src/es/galeriaapp/VentanaPrincipal.java
+++ src/es/galeriaapp/VentanaPrincipal.java
@@ -4,15 +4,32 @@ package es.galeriaapp;
 
 import java.awt.BorderLayout;
 
+import java.io.File;
+import java.util.List;
+
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class VentanaPrincipal extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	private File directorioPadre;
+	private List<File> listaImagenes;
+	private List<File> listaFavoritas;
+	private int indiceActual = 0;
+	private JLabel etiquetaImagen;
+	private javax.swing.Timer timerPresentacion;
 
+   private  JLabel etqietaNombreArchivo = new JLabel("");
+   
+   
 	public VentanaPrincipal()  {
 		super("Gestor de Imágenes");
 		
@@ -69,7 +86,7 @@ public class VentanaPrincipal extends JFrame{
 		itemSalir.addActionListener(e -> System.exit(0));
 		itemAcercaDe.addActionListener(e -> mostrarAcercaDe());
 		//itemFavoritos.addActionListener( e -> cargarFavortios("./img"));
-		
+		itemAbrirExplorador.addActionListener( e -> AbrirExplorador());
 		
 		
 		
@@ -90,6 +107,74 @@ public class VentanaPrincipal extends JFrame{
 	}
 
 
+
+	private void  AbrirExplorador() {
+	      //Permite seleccionar archivos o carpetas mediante un cuadro de diálogo. 
+        JFileChooser fileChooser = new JFileChooser();
+       
+        //Creamos un filtro para que el usuario sólo vea los archivos de imagen
+        FileNameExtensionFilter filtro =
+                new FileNameExtensionFilter("Archivos de Imagen", "jpg", "png", "gif", "bmp");  
+        fileChooser.setFileFilter(filtro);
+        
+        //el usuario sólo puede seleccionar archivos del filtro seleccionado
+        fileChooser.setAcceptAllFileFilterUsed(false); 
+
+        // hacer que el explorador se abra en la carpeta Imágenes del usuario actual
+        String rutaImagenes = System.getProperty("user.home") 
+                + File.separator 
+                + "Pictures";
+        
+        //Establece la carpeta inicial y crea un objeto File 
+        //que representa esa carpeta
+        fileChooser.setCurrentDirectory(new File(rutaImagenes));
+
+        //abre un cuadro de diálogo y espera a que el usuario realice una acción
+        //cancelar, cerrar, elegir archivo...
+        // this se refiere a VentanaPrincipal, es para que el JFileChooser 
+        //se abra centrado y asociado a esta ventana
+        int seleccion = fileChooser.showOpenDialog(this);
+
+        //si el usuario pulsó aceptar
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+
+            File archivoSeleccionado = fileChooser.getSelectedFile();
+            directorioPadre = archivoSeleccionado.getParentFile();
+             
+            // Cargar todas las imágenes de la carpeta
+            File[] archivos = directorioPadre.listFiles((dir, name) ->
+                    name.toLowerCase().endsWith(".jpg") ||
+                    name.toLowerCase().endsWith(".png") ||
+                    name.toLowerCase().endsWith(".gif") ||
+                    name.toLowerCase().endsWith(".bmp"));
+
+            if (archivos != null) {
+                listaImagenes = java.util.Arrays.asList(archivos);
+                indiceActual = listaImagenes.indexOf(archivoSeleccionado);
+            }             
+
+            ImageIcon imagenSeleccionada = new ImageIcon(archivoSeleccionado.getAbsolutePath());
+
+            if (imagenSeleccionada.getIconWidth() == -1) {
+                JOptionPane.showMessageDialog(this,
+                        "Error al cargar la imagen.",
+                        "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                iniciarVisorImagenes(imagenSeleccionada);
+            }
+        } // fin pulsar Aceptar
+		
+	}
+
+	/*private Object cargarFavortios(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+
+	private void iniciarVisorImagenes(ImageIcon imagenSeleccionada) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	private void mostrarAcercaDe() {
 		//debes pasarle un numero especifico de argumentos 
